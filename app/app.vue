af1b51f7cc72c57edd814bfa4d3a6325b4e9e8fd
Dustin Walker <dwtimp@gmail.com>
2016-10-31 17:14:08 -0500

    fixed vueify

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,35 @@
 <template lang="html">
+  <div class="app">
+    <div class="frame">
+      <div class="frame frame__left">
+        <h3>Build Your Monster</h3>
+      </div>
+
+        <div class="grid grid__right">
+
+        </div>
+
+          <div class="grid grid__favs">
+
+          </div>
+
+    </div>
+
+  </div>
 
 </template>
 
 <script>
 import Vue from 'vue';
-export default Vue.extend {
-}
+export default Vue.extend({
+  data() {
+    return {
+      monsters: [],
+
+    }
+  }
+})
+
 </script>
 
 <style lang="css">
