0ef6975756f37d29fd562a09a9a54d8238c974d8
Dustin Walker <dwtimp@gmail.com>
2016-11-30 19:31:40 -0600

    still working on

--- app/app.vue
+++ app/app.vue
@@ -1,43 +1,78 @@
 <template lang="html">
   <div class="app">
+
+
     <div class="sidebar">
-      <h3 class="sidebar__heading">Build Your Monster</h3>
+      <div class="sidebar__heading"><p>Build Your Monster</p></div>
         <div class="img__switcher">
-          <button class="btn"></button>
-          <i class="fa fa-chevron-left" aria-hidden="true"></i>
+          <button @click="updatePart('body', -1)" class="btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
           <div class="sidebar__frame">
-            <img class="sidebar__img" src="/monster/monsters-master/body-1.png" alt="" />
+            <img class="sidebar__img" :src="'/monster/' + monsterParts.body[selected.body] + '.png'" alt="" />
           </div>
-          <button class="btn"></button>
-          <i class="fa fa-chevron-right" aria-hidden="true"></i>
+          <button @click="updatePart('body', 1)" class="btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
+
         </div>
 
         <div class="img__switcher">
-          <button class="btn"></button>
-          <i class="fa fa-chevron-left" aria-hidden="true"></i>
+          <button @click="updatePart('mouth', -1)" class="btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
+
           <div class="sidebar__frame">
-            <img class="sidebar__img" src="/monster/monsters-master/eyes-1-crossed.png" alt="" />
+              <img class="sidebar__img" :src="'/monster/' + monsterParts.mouth[selected.mouth] + '.png'" alt="" />
           </div>
-          <button class="btn"></button>
-          <i class="fa fa-chevron-right" aria-hidden="true"></i>
+          <button @click="updatePart('mouth', 1)"class="btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
+
         </div>
 
         <div class="img__switcher">
-          <button class="btn"></button>
-          <i class="fa fa-chevron-left" aria-hidden="true"></i>
+          <button @click="updatePart('eyes' -1)" class="btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
+
           <div class="sidebar__frame">
-            <img class="sidebar__img" src="/monster/monsters-master/mouth-1.png" alt="" />
+            <img class="sidebar__img" :src="'/monster/' + monsterParts.eyes.[selected.eyes] + '.png'" alt="" />
           </div>
-          <button class="btn"></button>
-          <i class="fa fa-chevron-right" aria-hidden="true"></i>
+          <button @click="updatePart('eyes' 1)" class="btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
+
         </div>
 
-      <form class="sidebar__form" action="index.html" method="post">
-        <input class="sidebar__input" type="button" name="name" placeholder="Name Your Creation">
+      <form class="sidebar__form" @submit.prevent="saveMonster">
+        <input class="sidebar__input" type="text"  placeholder="Name Your Creation">
         <button class="sidebar__submit">Save Favorite!</button>
       </form>
     </div>
+
+
+
+    <div class="app__hero">
+      <div class="app__hero--content">
+        <div class="big">
+          <img class="big__img" :src="'/monster/' + monsterParts.body[selected.body] + '.full.png'"  alt="" />
+          <img class="big__img" :src="'/monster/' + monsterParts.mouth[selected.mouth] + '.full.png'"  alt="" />
+          <img class="big__img" :src="'/monster/' + monsterParts.eyes[selected.eyes] + '.full.png'"  alt="" />
+
+
+        </div>
+      </div>
+
+
+
+<div class="favs">
+  <div class="favs__grid">
+  <div class="favs__row" v-for="favorite in favorites">
+    <div class="favs__frame">
+      <img class="favs__img" :src="'/monster/' + monsterParts.body[favorite.body] + '.full.png'" alt="" />
+      <img class="favs__img" :src="'/monster/' + monsterParts.mouth[favorite.mouth] + '.full.png'" alt="" />
+      <img class="favs__img" :src="'/monster/' + monsterParts.eyes[favorite.eyes] + '.full.png'" alt="" />
+
+
+    </div>
+    <div class="favs__name"><p>Name</p></div>
   </div>
+</div>
+</div>
+</div>
+
+
+
+</div>
 
 
 
@@ -51,12 +86,50 @@ import Vue from 'vue';
 export default Vue.extend({
     data() {
         return {
-            monsterParts: [],
-            form: {
-              name: 'monster-name'
-            },
+          selected: {
+            body: 0,
+            mouth: 0,
+            eyes: 0,
+            name: '',
+          },
+          monsterParts,
+          favorites: [],
         };
     },
+  mounted() {
+    this.getFavorites();
+  },
+  methods: {
+    getFavorites(){
+      fetch(`http://tiny-tn.herokuapp.com/collections/dw-monsters`)
+      .then((r) => r.json())
+      .then((favorites) => {
+        this.favorites = favorites;
+      });
+    },
+    updatePart(bodyPart, difference = 1) {
+      this.selected[bodyPart] = (this.selected[bodyPart] + difference) % this.monsterParts[bodyPart].lenght;
+    },
+    saveMonster(){
+      fetch(`http://tiny-tn.herokuapp.com/collections/dw-monsters`{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.selected)
+      })
+      .then((r) => r.json())
+      .then((favorite) => {
+        this.favorites = [favorite, ...this.favorites];
+        this.selected = {
+          name: '',
+          body: 0,
+          eyes: 0,
+          mouth: 0,
+        };
+      });
+    },
+  },
 });
 </script>
 
